ba673bd139dfbe9be6f274580a312bf43deec7fd
ErgeibiMed <ergeibi.mohamed@gmail.com>
2024-09-17 20:36:35 +0100

    still working on the lexer to improve it

--- src/lexer.rs
+++ src/lexer.rs
@@ -19,7 +19,11 @@ impl<'de> Lexer<'de> {
 impl<'de> Iterator for Lexer<'de> {
     type Item = Result<Token<'de>, Error>;
     fn next(&mut self) -> Option<Self::Item> {
-        loop{
+        if self.byte>=self.whole.len(){
+            println!("reached the end of the file");
+            return Some(Ok(Token::EOF))
+        }
+        loop {
         let mut chars = self.rest.chars();
         let c = chars.next()?;
         self.byte += c.len_utf8();
@@ -33,7 +37,7 @@ impl<'de> Iterator for Lexer<'de> {
             IDent,
         }
          let started = match c {
-            ' ' => {continue;  },//no action
+            ' '=> return Some(Ok(Token::Whitespace)),
             '$' => return Some(Ok(Token::DollarSign)),
             '+' => return Some(Ok(Token::Addition)),
             '-' => return Some(Ok(Token::Substraction)),
@@ -59,37 +63,39 @@ impl<'de> Iterator for Lexer<'de> {
             '{' => return Some(Ok(Token::LeftBracket)),
             '}' => return Some(Ok(Token::RightBracket)),
             '~' => {
-                    let endline= self.rest.find('\n').unwrap();
-                     let comment = &self.rest[c.len_utf8()+1..endline+c.len_utf8()-1];
-                     self.byte+=comment.len();
-                    self.rest=&self.rest[self.byte..];
-                    //self.byte+=
-                    return Some(Ok(Token::Tilde))
-                },
+                let endline= self.rest.find('\n').unwrap();
+                let comment = &self.rest[c.len_utf8()..endline];
+                self.byte+=endline+c.len_utf8();
+                self.rest=&self.whole[c.len_utf8()+self.byte..];
+                //self.byte+=
+                return Some(Ok(Token::Tilde(&comment)))
+            },
             '?' => Started::Input,                  //return Some(Ok(Token::QuestionMark)),
             '"' =>  Started::String,                //return Some(Ok(Token::QuotationMark)),
             '\'' => Started::Character,                // return Some(Ok(Token::Apostrophe)),
             '0'..='9' => Started::Number,
             'a'..='z'|'A'..='Z' => Started::IDent,
-            _ => unreachable!("this token is not valid"),
+            _ => { //println!("End of File {}",c);
+                    continue
+                },
         };
 
             match started{
                 Started::String =>{ if let Some(end)= self.rest.find('"'){
-                let literal=&self.rest[c.len_utf8()-1..end+c.len_utf8()-1];
-                self.byte+=literal.len();
-                self.rest=&self.rest[self.byte..];
+                let literal=&self.rest[c.len_utf8()-1..end];
+                self.byte+=literal.len()+c.len_utf8();
+                self.rest=&self.whole[self.byte..];
 
                 return Some(Ok(Token::Literal(literal)));
             }else {
-
+                       //miette::errror here not implemented yet!
                     }
                 },
                 Started::IDent =>{
                        let end= self.rest.find(' ').unwrap();
-                      let identifier= &self.rest[c.len_utf8()..end+c.len_utf8()-1];
-                      self.byte+=identifier.len();
-                      self.rest=&self.rest[self.byte..];
+                      let identifier= &self.rest[c.len_utf8()-1..end];
+                      self.byte+=identifier.len()+c.len_utf8();
+                      self.rest=&self.whole[self.byte-1..];
                     return Some(Ok(Token::VarIdentifier(identifier)));
                 },
                Started::Character => return Some(Ok(Token::Char(c))),
@@ -103,8 +109,8 @@ impl<'de> Iterator for Lexer<'de> {
                        for i in 0..rst.len(){
                       whole.push(rst[i]);
                 }
-                     self.byte+=whole.len();
-                     self.rest=&self.rest[self.byte..];
+                     self.byte+=whole.len()-1;
+                     self.rest=&self.whole[self.byte..];
                    let whole = whole.iter().collect::<String>();
 
                 return Some(Ok(Token::Number(whole.parse::<usize>().unwrap())));
@@ -118,6 +124,8 @@ impl<'de> Iterator for Lexer<'de> {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Token<'de> {
+    EOF,
+    Whitespace,
     DollarSign,         //         $
     Addition,           //         +
     Substraction,       //         -
@@ -141,7 +149,7 @@ pub enum Token<'de> {
     Comma,              //         ,
     SemiColon,          //         ;
     LeftBracket,        //         {
-    Tilde,
+    Tilde(&'de str),
     RightBracket, //         }
     VarIdentifier(&'de str),
     Literal(&'de str),
@@ -154,31 +162,33 @@ pub enum Token<'de> {
 impl<'de> Display for Token<'de> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Token::DollarSign => write!(f, " $ "),
-            Token::Addition => write!(f, " + "),
-            Token::Substraction => write!(f, " - "),
-            Token::Multiplication => write!(f, " * "),
-            Token::Division => write!(f, " / "),
-            Token::AntiSlash => write!(f, r" \ "),
-            Token::Bang => write!(f, " ! "),
-            Token::Colon => write!(f, " : "),
-            Token::Point => write!(f, " . "),
-            Token::LessThan => write!(f, " < "),
-            Token::Equal => write!(f, " = "),
-            Token::GreaterThan => write!(f, " > "),
-            Token::LeftSquareBracket => write!(f, " [ "),
-            Token::RightSquareBracket => write!(f, " ] "),
-            Token::LeftParnathesis => write!(f, " ( "),
-            Token::RightParnathesis => write!(f, " ) "),
-            Token::Caret => write!(f, " ^ "),
-            Token::Pound => write!(f, " # "),
-            Token::AtSign => write!(f, " @ "),
-            Token::Ampersand => write!(f, " % "),
-            Token::Comma => write!(f, " , "),
-            Token::SemiColon => write!(f, " ; "),
-            Token::LeftBracket => write!(f, " {{ "),
-            Token::Tilde => write!(f, " ~ "),
-            Token::RightBracket => write!(f, " }} "),
+            Token::EOF=> write!(f,"end of file"),
+            Token::DollarSign => write!(f, "$"),
+            Token::Whitespace => write!(f, "Whitespace"),
+            Token::Addition => write!(f, "+"),
+            Token::Substraction => write!(f, "-"),
+            Token::Multiplication => write!(f, "*"),
+            Token::Division => write!(f, "/"),
+            Token::AntiSlash => write!(f, r"\"),
+            Token::Bang => write!(f, "!"),
+            Token::Colon => write!(f, ":"),
+            Token::Point => write!(f, "."),
+            Token::LessThan => write!(f, "<"),
+            Token::Equal => write!(f, "="),
+            Token::GreaterThan => write!(f, ">"),
+            Token::LeftSquareBracket => write!(f, "["),
+            Token::RightSquareBracket => write!(f, "]"),
+            Token::LeftParnathesis => write!(f, "("),
+            Token::RightParnathesis => write!(f, ")"),
+            Token::Caret => write!(f, "^"),
+            Token::Pound => write!(f, "#"),
+            Token::AtSign => write!(f, "@"),
+            Token::Ampersand => write!(f, "%"),
+            Token::Comma => write!(f, ","),
+            Token::SemiColon => write!(f, ";"),
+            Token::LeftBracket => write!(f, "{{"),
+            Token::Tilde(s) => write!(f, "~comment_is->{}",s),
+            Token::RightBracket => write!(f, "}}"),
             Token::Literal(s) => write!(f,"\"{}\"",s),
             Token::VarIdentifier(i) => write!(f,"{}",i),
             Token::Char(c) => write!(f,"'{}",c),
